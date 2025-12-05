efcb27fa2b9a348e6899e144310443a95303c4c1
Oliver Soeser <o.soeser@icloud.com>
2025-12-05 23:04:33 +0000

    definitions

--- Asympt/Basic.lean
+++ Asympt/Basic.lean
@@ -1,1 +1,14 @@
-def hello := "world"
+def bigO (f g : Nat → Nat) : Prop :=
+  ∃c, ∃n₀, ∀n, n ≥ n₀ → f n ≤ c * g n
+
+def bigOmega (f g : Nat → Nat) : Prop :=
+  ∃c, ∃n₀, ∀n, n ≥ n₀ → c * g n ≤ f n
+
+def bigTheta (f g : Nat → Nat) : Prop :=
+  ∃c₁, ∃c₂, ∃n₀, ∀n, n ≥ n₀ → c₁ * g n ≤ f n ∧ f n ≤ c₂ * g n
+
+def littleO (f g : Nat → Nat) : Prop :=
+  ∀c, ∃n₀, ∀n, n ≥ n₀ → f n < c * g n
+
+def littleOmega (f g : Nat → Nat) : Prop :=
+  ∀c, ∃n₀, ∀n, n ≥ n₀ → c * g n < f n
