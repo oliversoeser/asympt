55070a7b0d0ac89a5cf26a0a3b0c87a12cf056a1
Oliver Soeser <o.soeser@icloud.com>
2025-12-09 21:16:53 +0000

    restructuring and example 3

--- Asympt/Basic.lean
+++ Asympt/Basic.lean
@@ -1,14 +1,1 @@
-def bigO (f g : Nat → Nat) : Prop :=
-  ∃c > 0, ∃n₀, ∀n ≥ n₀, f n ≤ c * g n
-
-def bigOmega (f g : Nat → Nat) : Prop :=
-  ∃c > 0, ∃n₀, ∀n ≥ n₀, c * g n ≤ f n
-
-def bigTheta (f g : Nat → Nat) : Prop :=
-  ∃c₁ > 0, ∃c₂ > 0, ∃n₀, ∀n ≥ n₀, c₁ * g n ≤ f n ∧ f n ≤ c₂ * g n
-
-def littleO (f g : Nat → Nat) : Prop :=
-  ∀c > 0, ∃n₀, ∀n ≥ n₀, f n < c * g n
-
-def littleOmega (f g : Nat → Nat) : Prop :=
-  ∀c > 0, ∃n₀, ∀n ≥ n₀, c * g n < f n
+import Asympt.Defs
