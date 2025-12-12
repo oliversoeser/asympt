694e85cad828247cebf948f5763c3b9ddb295531
Oliver Soeser <o.soeser@icloud.com>
2025-12-12 13:01:47 +0000

    explicit poly list

--- Asympt/Polynomial.lean
+++ Asympt/Polynomial.lean
@@ -1,15 +1,13 @@
+import Asympt.Defs
+
 def toPolyFromDegree (l : List Nat) (d : Nat) : Nat → Nat := λx => match l with
   | List.nil => 0
   | List.cons c t => c * x ^ d + toPolyFromDegree t (d + 1) x
 
 def toPoly (l : List Nat) : Nat → Nat := toPolyFromDegree l 0
 
-def isPoly (f : Nat → Nat) : Prop := ∃l, f = toPoly l
-
 class Poly (f : Nat → Nat) where
-  poly : isPoly f
-
--- Alternative Typeclass
-class PolyConcrete (f : Nat → Nat) where
   list : List Nat
   poly : f = toPoly list
+
+theorem poly_degree_concrete (f : Nat → Nat) [h : Poly f] : bigO f (λn => n ^ h.list.length) := sorry
