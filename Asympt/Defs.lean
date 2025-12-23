00f1c37986a5efa8744392f17bf9be9cbff4db65
Oliver Soeser <o.soeser@icloud.com>
2025-12-23 16:49:24 +0000

    additional definitions

--- Asympt/Defs.lean
+++ Asympt/Defs.lean
@@ -1,5 +1,9 @@
+-- Fundamental Definitions - O and o
 def bigO (f g : Nat → Nat) : Prop :=
   ∃c, ∃n₀, ∀n ≥ n₀, f n ≤ c * g n
+
+def littleO (f g : Nat → Nat) : Prop :=
+  ∀c > 0, ∃n₀, ∀n ≥ n₀, f n ≤ c * g n
 
 class BigO (f : Nat → Nat) (g : outParam (Nat → Nat)) where
   big_o : bigO f g
@@ -9,3 +13,11 @@ class BigOConcrete (f : Nat → Nat) (g : outParam (Nat → Nat)) where
   c : Nat
   n₀ : Nat
   big_o : ∀n ≥ n₀, f n ≤ c * g n
+
+class LittleO (f : Nat → Nat) (g : outParam (Nat → Nat)) where
+  little_o : littleO f g
+
+-- Derived Definitions - Omegas and Theta
+def bigOmega (f g : Nat → Nat) : Prop := bigO g f
+def littleOmega (f g : Nat → Nat) : Prop := littleO g f
+def bigTheta (f g : Nat → Nat) : Prop := bigO f g ∧ bigOmega f g
