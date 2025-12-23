a8bd4346ee4891ec15d5256fe11e716654f5a6bd
Oliver Soeser <o.soeser@icloud.com>
2025-12-23 18:53:47 +0000

    additional typeclasses

--- Asympt/Defs.lean
+++ Asympt/Defs.lean
@@ -8,16 +8,19 @@ def littleO (f g : Nat → Nat) : Prop :=
 class BigO (f : Nat → Nat) (g : outParam (Nat → Nat)) where
   big_o : bigO f g
 
--- Alternative Typeclass
-class BigOConcrete (f : Nat → Nat) (g : outParam (Nat → Nat)) where
-  c : Nat
-  n₀ : Nat
-  big_o : ∀n ≥ n₀, f n ≤ c * g n
-
 class LittleO (f : Nat → Nat) (g : outParam (Nat → Nat)) where
   little_o : littleO f g
 
 -- Derived Definitions - Omegas and Theta
 def bigOmega (f g : Nat → Nat) : Prop := bigO g f
 def littleOmega (f g : Nat → Nat) : Prop := littleO g f
-def bigTheta (f g : Nat → Nat) : Prop := bigO f g ∧ bigOmega f g
+def bigTheta (f g : Nat → Nat) : Prop := bigO f g ∧ bigO g f
+
+class BigOmega (f : Nat → Nat) (g : outParam (Nat → Nat)) where
+  big_omega : bigOmega f g
+
+class LittleOmega (f : Nat → Nat) (g : outParam (Nat → Nat)) where
+  little_omega : bigOmega f g
+
+class BigTheta (f : Nat → Nat) (g : outParam (Nat → Nat)) where
+  big_theta : bigTheta f g
