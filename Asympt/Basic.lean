8509eed91cc4a513f414e61ef545dd5e9ae8cdea
Oliver Soeser <o.soeser@icloud.com>
2025-12-25 16:26:43 +0000

    little o lemmas

--- Asympt/Basic.lean
+++ Asympt/Basic.lean
@@ -3,7 +3,7 @@ def bigO (f g : Nat → Nat) : Prop :=
   ∃c, ∃n₀, ∀n ≥ n₀, f n ≤ c * g n
 
 def littleO (f g : Nat → Nat) : Prop :=
-  ∀c₁ > 0,∀c₂ > 0, ∃n₀, ∀n ≥ n₀, c₁ * f n ≤ c₂ * g n
+  ∀c > 0, ∃n₀, ∀n ≥ n₀, f n < c * g n
 
 class BigO (f : Nat → Nat) (g : outParam (Nat → Nat)) where
   big_o : bigO f g
--- Asympt/LittleO.lean
+++ Asympt/LittleO.lean
@@ -7,7 +7,30 @@ namespace LittleO
 theorem irrefl (f : Nat → Nat) : ¬littleO f f := by
   intro h
   unfold littleO at h
-  sorry
+  have ⟨n₀,h⟩ := h 1 Nat.one_pos
+  have h := h n₀
+  simp at h
+
+theorem asymm (f g : Nat → Nat) (h : littleO f g) : ¬littleO g f := by
+  intro h'
+  have ⟨n₁,h⟩ := h 1 Nat.one_pos
+  have ⟨n₂,h'⟩ := h' 1 Nat.one_pos
+  let n₀ := max n₁ n₂
+  have h := h n₀ (max_le_iff.mp (le_refl n₀)).1
+  have h' := h' n₀ (max_le_iff.mp (le_refl n₀)).2
+  simp_all
+  exact Nat.lt_irrefl (f n₀) (lt_trans h h')
+
+theorem trans {f g h : Nat → Nat} (h₁ : littleO f g) (h₂ : littleO g h) : littleO f h := by
+  intro c hc
+  have ⟨n₁,h₁⟩ := h₁ c hc
+  have ⟨n₂,h₂⟩ := h₂ 1 Nat.one_pos
+  exists (max n₁ n₂)
+  intro n hn
+  have h₁ := h₁ n (max_le_iff.mp hn).1
+  have h₂ := h₂ n (max_le_iff.mp hn).2
+  simp at h₂
+  exact Nat.lt_trans h₁ (Nat.mul_lt_mul_of_pos_left h₂ hc)
 
 instance lt : LT (Nat → Nat) := ⟨littleO⟩
 
