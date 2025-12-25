4b72a47a937968168eaf6013e5d234fba0b635db
Oliver Soeser <o.soeser@icloud.com>
2025-12-25 13:35:18 +0000

    make little o work for integers

--- Asympt/LittleO.lean
+++ Asympt/LittleO.lean
@@ -4,26 +4,13 @@ open Std
 
 namespace LittleO
 
-theorem irrefl (f : Nat → Nat) : ¬littleO f f := sorry
+theorem irrefl (f : Nat → Nat) : ¬littleO f f := by
+  intro h
+  unfold littleO at h
+  sorry
 
 instance lt : LT (Nat → Nat) := ⟨littleO⟩
 
-instance lawful_lt : LawfulOrderLT (Nat → Nat) := ⟨by
-  intro a b
-  apply Iff.intro
-  · intro h
-    unfold BigO.le
-    unfold lt at h
-    simp_all
-    and_intros
-    · exists 1
-      exact h 1 Nat.one_pos
-    · sorry
-  · intro ⟨h₁,h₂⟩
-    unfold lt
-    unfold BigO.le at h₁ h₂
-    simp_all
-    sorry
-⟩
+instance lawful_lt : LawfulOrderLT (Nat → Nat) := ⟨sorry⟩
 
 end LittleO
