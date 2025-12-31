9c9a7e248f29f7380467a4ea4bf537f8a473e1f7
Oliver Soeser <o.soeser@icloud.com>
2025-12-31 15:26:02 +0100

    docs setup

--- Asympt.lean
+++ Asympt.lean
@@ -1,3 +1,5 @@
 -- This module serves as the root of the `Asympt` library.
 -- Import modules here that should be built as part of the library.
-import Asympt.Basic
+import Asympt.APos
+import Asympt.BigTheta
+import Asympt.LittleO
