2d4e05bae770034232830c9bd277c68670193d39
Oliver Soeser <o.soeser@icloud.com>
2026-01-04 11:13:46 +0100

    remove tactic experiment

--- Asympt/Lift.lean
+++ Asympt/Lift.lean
@@ -1,5 +1,3 @@
-import Asympt.Tactics
-
 namespace Lift
 
 open Lean.Grind
@@ -19,11 +17,22 @@ instance : HPow (A → B) Nat (A → B) where hPow f n := λx => (f x) ^ n
 instance : NatCast (A → B) where natCast n := λ_ => sr.natCast.natCast n
 instance {n : Nat} : OfNat (A → B) n where ofNat := λ_ => (sr.ofNat n).ofNat
 
-instance lift_semiring_fun : Semiring (A → B) := by
-  constructor
-  all_goals (intros; funext; try struct_exact sr)
-  · sorry
-  · sorry
-  · sorry
+instance lift_semiring_fun : Semiring (A → B) where
+  add_zero := by intro; funext; exact sr.add_zero _
+  add_comm := by intros; funext; exact sr.add_comm _ _
+  add_assoc := by intros; funext; exact sr.add_assoc _ _ _
+  mul_assoc := by intros; funext; exact sr.mul_assoc _ _ _
+  mul_one := by intro; funext; exact sr.mul_one _
+  one_mul := by intro; funext; exact sr.one_mul _
+  left_distrib := by intros; funext; exact sr.left_distrib _ _ _
+  right_distrib := by intros; funext; exact sr.right_distrib _ _ _
+  zero_mul := by intro; funext; exact sr.zero_mul _
+  mul_zero := by intro; funext; exact sr.mul_zero _
+  pow_zero := by intro; funext; exact sr.pow_zero _
+  pow_succ := by intros; funext; exact sr.pow_succ _ _
+  ofNat_succ := by intros; funext; exact sr.ofNat_succ _
+  ofNat_eq_natCast := by intros; funext; exact sr.ofNat_eq_natCast _
+  nsmul_eq_natCast_mul := by intros; funext; exact sr.nsmul_eq_natCast_mul _ _
+
 
 end Semiring
